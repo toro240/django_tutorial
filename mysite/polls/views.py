a244834b6f690173ce7a6f42d54b8c6783b9b1c6
西尾真 <toron240@gmail.com>
2020-08-14 14:08:04 +0900

    test rebase 2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,6 +23,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/index.html'
+    template_name = 'polls/index.html'
+    model = Question
+    template_name = 'polls/index.html'
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
