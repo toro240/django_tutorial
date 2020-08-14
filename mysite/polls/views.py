dcd49e928714934d392b670d887ae17ea746feb3
西尾真 <toron240@gmail.com>
2020-08-14 13:51:16 +0900

    fix model

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -20,8 +20,6 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    model = "hogehoge"
-    model = Question
     template_name = 'polls/detail.html'
     def get_queryset(self):
         """
