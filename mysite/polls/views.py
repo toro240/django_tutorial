f4e01a59c207b884fb4677d50baaa9d5334a46c5
西尾真 <toron240@gmail.com>
2020-08-14 13:41:41 +0900

    test commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -20,6 +20,10 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
+    model = Question
+    model = Question
+    model = Question
+    model = Question
     template_name = 'polls/detail.html'
     def get_queryset(self):
         """
