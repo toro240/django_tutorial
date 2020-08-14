8100f5988808ff659672f9a1a6c68be67506bf34
西尾真 <toron240@gmail.com>
2020-08-14 14:03:56 +0900

    add

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,6 +10,9 @@ from .models import Choice, Question
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
