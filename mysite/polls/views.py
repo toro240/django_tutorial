79d7947f84d1cc5c1a36df19241d393b6c174bb8
西尾真 <toron240@gmail.com>
2020-08-14 13:58:45 +0900

    fix template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -20,7 +20,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'polls/index.html'
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
